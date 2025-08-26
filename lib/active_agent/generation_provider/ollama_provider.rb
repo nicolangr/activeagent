b0bc1752b15e9edb31b713cb8d15016e152bf72b
Nicola Moschioni <nicola.ngr@gmail.com>
2025-08-26 14:22:29 +0200

    added ollama embeddigs parameters for nomic-embed-text

--- lib/active_agent/generation_provider/ollama_provider.rb
+++ lib/active_agent/generation_provider/ollama_provider.rb
@@ -9,8 +9,18 @@ module ActiveAgent
         @access_token ||= config["api_key"] || config["access_token"] || ENV["OLLAMA_API_KEY"] || ENV["OLLAMA_ACCESS_TOKEN"]
         @model_name = config["model"]
         @host = config["host"] || "http://localhost:11434"
-        @client = OpenAI::Client.new(uri_base: @host, access_token: @access_token, log_errors: true)
+        @api_version = config["api_version"] || "v1"
+        @client = OpenAI::Client.new(uri_base: @host, access_token: @access_token, log_errors: true, api_version:@api_version )
       end
+
+      def embeddings_parameters(input: prompt.message.content, model: "text-embedding-3-large")
+        {
+          model: self.config["embedding_model"] || model,
+          prompt: input
+        }
+      end
+
+
     end
   end
 end
